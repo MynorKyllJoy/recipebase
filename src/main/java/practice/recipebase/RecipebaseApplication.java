e61e8d6330acbf836f67c15c958697b1a3c734c1
MynorKyllJoy <m.k.huynh@gmx.de>
2025-06-14 21:33:00 +0200

    fix: Add RecipebaseApplication main function
    RecipebaseApplication containing the main function was missing from
    previous commit.

---
+++ src/main/java/practice/recipebase/RecipebaseApplication.java
@@ -0,0 +1,13 @@
+package practice.recipebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipebaseApplication.class, args);
+	}
+
+}
