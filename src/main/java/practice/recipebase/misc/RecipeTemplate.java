64e8d254ba8b3fa91dea6d5b3b8bf895e9ab3e62
MynorKyllJoy <m.k.huynh@gmx.de>
2025-06-25 03:11:18 +0200

    refactor: improve comments in RecipeTemplate

--- src/main/java/practice/recipebase/misc/RecipeTemplate.java
+++ src/main/java/practice/recipebase/misc/RecipeTemplate.java
@@ -14,12 +14,17 @@ abstract class RecipeTemplate {
     // Template pattern since I want to save scraped and self-made recipes in the database
     public Recipe createRecipe() throws WrongTokenTypeException {
         try {
+            // get simple information
             String title = this.getTitle();
             String description = this.getDescription();
             String source = this.getSource();
+
+            // process ingredient information
             List<String> ingredientInfos = this.getIngredientInfos();
             ingredientInfos = this.cleanIngredientInfos(ingredientInfos);
+            System.out.println(ingredientInfos);
             List<Requirement> requiredIngredients = this.getRequirements(ingredientInfos);
+            // get instruction information
             List<String> instructions = this.getInstructions();
             return new Recipe(title, description, source, requiredIngredients, instructions);
         } catch (IOException ex) {
@@ -28,18 +33,21 @@ abstract class RecipeTemplate {
     }
 
     private List<String> cleanIngredientInfos(List<String> ingredientInfos) {
+        // transform string into a format that works with parser
         List<String> cleanedIngredients = new ArrayList<>();
         for(String info : ingredientInfos) {
             IngredientCleaner cleaner = new IngredientCleaner(info);
             String cleanedInfo = cleaner
                     .removeOxfordComma()
+                    .fixHybridFractions() // turns 1½ and 1 ½ into 1+½
                     .replaceUnicode() // transform all Unicode characters we want to preserve
                     .removeNonASCII() // remove all remaining Unicode characters
+                    .removeUnneededComments()
                     .cleanBrackets()
                     .removeEmptyBrackets()
                     .replaceAndSlashOrWithOr()
                     .replaceSlashesNextToWordsWithComma()
-                    .enumerationCommaToOr()
+                    .replaceEnumerationCommaToOr()
                     .useHyphenForWords() // add hyphen as Unicode character
                     .replaceToWithMinusForRanges()
                     .getCleanedIngredient();
@@ -49,10 +57,10 @@ abstract class RecipeTemplate {
     }
 
     private List<Requirement> getRequirements(List<String> ingredientInfos) throws WrongTokenTypeException {
+        // tokenize and parse string to get the necessary requirements
         List<Requirement> requiredIngredients = new ArrayList<>();
         for(String ingredientString : ingredientInfos) {
             IngredientTokenizer tokenizer = new IngredientTokenizer(ingredientString);
-            System.out.println(tokenizer.toList());
             IngredientParser parser = new IngredientParser(tokenizer, 0);
             requiredIngredients.addAll(parser.parse().interpret().getRequirements());
         }
