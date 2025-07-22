e744fb2fe2caa1173be610d6c346ec5fbc48ceef
MynorKyllJoy <m.k.huynh@gmx.de>
2025-07-22 13:32:27 +0200

    refactor: turn controllers into REST controllers
    Turn LoginController and RecipeController into REST controllers.

--- spring-backend/src/main/java/practice/recipebase/controller/LoginController.java
+++ spring-backend/src/main/java/practice/recipebase/controller/LoginController.java
@@ -1,34 +1,24 @@
 package practice.recipebase.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import practice.recipebase.model.AppUser;
 import practice.recipebase.service.AppUserService;
 
-@Controller
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins="http://localhost:5173")
 public class LoginController {
     @Autowired
     AppUserService userService;
 
-    @GetMapping("/login")
-    public String showLoginForm(Model model) {
-        model.addAttribute("user", new AppUser());
-        return "login";
-    }
-
-    @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
-        model.addAttribute("user", new AppUser());
-        return "register";
+    @PostMapping("/login")
+    public String login(@RequestBody AppUser user) {
+        return userService.verify(user);
     }
 
     @PostMapping("/register")
-    public String registerNewUser(@ModelAttribute("user") AppUser newUser) {
-        userService.register(newUser);
-        return "index";
+    public AppUser register(@RequestBody AppUser newUser) {
+        return userService.register(newUser);
     }
 }
