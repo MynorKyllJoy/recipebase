878f8c10717a63cac60ed17182dba2413ba680a0
MynorKyllJoy <m.k.huynh@gmx.de>
2025-09-07 18:17:44 +0200

    ci: add coverage summary to pull request
    test: add coverage percentage threshold, so ci will fail if the coverage
    is below the set percentage

--- react-frontend/vitest.config.ts
+++ react-frontend/vitest.config.ts
@@ -7,8 +7,15 @@ export default defineConfig({
         setupFiles: "tests/setup.ts",
         coverage: {
             provider: "v8",
-            reporter: ["text", "json", "html"],
-            reportsDirectory: "./tests/unit/coverage"
+            reporter: ["text", "json", 'json-summary', "html"],
+            reportsDirectory: "./tests/unit/coverage",
+            reportOnFailure: true,
+            thresholds: {
+                lines: 70,
+                branches: 70,
+                functions: 70,
+                statements: 70
+            }
         }
     }
 });
